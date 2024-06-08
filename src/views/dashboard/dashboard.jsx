ae7632cfcf0114a0e3eefe13b562eaeeb879c826
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-08 16:47:36 -0400

    Updated dashbaord to be responsive on mobile

--- src/views/dashboard/dashboard.jsx
+++ src/views/dashboard/dashboard.jsx
@@ -9,18 +9,18 @@ import AppBar from "../../components/specific/dashboard/appBar";
 const Home = () => {
     return (
         <>
-        <AppBar />        
-        <Grid container spacing={2} sx={{ height: '100vh', width: '100vw'}}>
-            <Grid item xs={12}> {/* Ticker */}
-                <TradingViewTicker />
+            <AppBar />        
+            <Grid container spacing={2} sx={{ height: '100vh', width: '100vw', overflowY: 'auto' }}>
+                <Grid item xs={12}> {/* Ticker */}
+                    <TradingViewTicker />
+                </Grid>
+                <Grid item xs={12} md={8} sx={{ order: { xs: 2, md: 1 } }}> {/* Main Content */}
+                    {/* Add  main content here */}
+                </Grid>
+                <Grid item xs={12} md={4} sx={{ order: { xs: 3, md: 2 } }}> {/* Timeline */}
+                    <TradingViewTimeline />
+                </Grid>
             </Grid>
-            <Grid item xs={12} md={8} sx={{ order: { xs: 2, md: 2 } }}> {/* Main Content */}
-            
-            </Grid>
-            <Grid item xs={12} md={4} sx={{ order: { xs: 3, md: 3 } }}> {/* Timeline */}
-                <TradingViewTimeline />
-            </Grid>
-        </Grid>
         </>
     );
 }
