09c34a11dc145c8bf1ddd18ba4808285dcb542b0
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-09 23:33:25 -0400

    Updated tile layout and style. Adjusted index.css and removed overflow

--- src/components/specific/dashboard/tileModules.jsx
+++ src/components/specific/dashboard/tileModules.jsx
@@ -3,18 +3,33 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Image } from '@mui/icons-material';
+import { colors } from '@mui/material';
 
-const Tile = ({ title, description, actionText, onActionClick }) => {
+const Tile = ({ icon: Icon, title, description, actionText, onActionClick }) => {
     return (
-        <Card >
+        <Card
+        sx={{
+            backgroundColor: '#131722',
+            color: 'white',
+            display: 'flex',
+
+        }}
+        >
             <CardContent>
-                <Typography variant="h5" component="div">
+                    {Icon && <Icon sx={{ fontSize: 40, mb: 2, color: '#f1c50e'}} />}                <Typography variant="h5" component="div">
                     {title}
                 </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <Typography sx={{ mb: 1.5 }} color="white">
                     {description}
                 </Typography>
-                <Button variant="contained" onClick={onActionClick}>{actionText}</Button>
+                <Button
+                 variant="contained"
+                 sx={{ 
+                    backgroundColor: '#f1c50e',
+                    color: 'white' }} 
+                 onClick={onActionClick}
+                  >{actionText}</Button>
             </CardContent>
         </Card>
     );
--- src/views/dashboard/dashboard.jsx
+++ src/views/dashboard/dashboard.jsx
@@ -6,96 +6,108 @@ import TradingViewTicker from '../../components/specific/dashboard/ticker';
 import AppBar from '../../components/specific/dashboard/appBar';
 import Tile from '../../components/specific/dashboard/tileModules';
 import Sidebar from '../../components/specific/dashboard/sidebar';
-import TradingViewTimeline from '../../components/specific/dashboard/tradingViewTimeline';  
+import TradingViewTimeline from '../../components/specific/dashboard/tradingViewTimeline';
+import SchoolIcon from '@mui/icons-material/School';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import QuizIcon from '@mui/icons-material/Quiz';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 
 const Home = () => {
-  const tileData = [
-    {
-      title: 'Learning Pathways',
-      description: 'Personalized learning paths to guide your trading education.',
-      actionText: 'Explore',
-      onActionClick: () => {
-        // Handle click event for Learning Pathways
-      },
-    },
-    {
-      title: 'Daily Challenges',
-      description: 'Participate in daily trading challenges to test your skills.',
-      actionText: 'Join Now',
-      onActionClick: () => {
-        // Handle click event for Daily Challenges
-      },
-    },
-    {
-      title: 'Quizzes and Assessments',
-      description: 'Test your knowledge with quizzes and assessments.',
-      actionText: 'Start Quiz',
-      onActionClick: () => {
-        // Handle click event for Quizzes and Assessments
-      },
-    },
-    {
-      title: 'Market Sentiment Analysis',
-      description: 'Stay updated with real-time market sentiment analysis.',
-      actionText: 'View Analysis',
-      onActionClick: () => {
-        // Handle click event for Market Sentiment Analysis
-      },
-    },
-  ];
+	const tileData = [
+		{
+			title: 'Learning Pathways',
+			description:
+				'Personalized learning paths to guide your trading education.',
+			actionText: 'Explore',
+			icon: SchoolIcon,
+			onActionClick: () => {
+				// Handle click event for Learning Pathways
+			},
+		},
+		{
+			title: 'Daily Challenges',
+			description:
+				'Participate in daily trading challenges to test your skills.',
+			actionText: 'Join Now',
+			icon: EmojiEventsIcon,
+			onActionClick: () => {
+				// Handle click event for Daily Challenges
+			},
+		},
+		{
+			title: 'Quizzes and Assessments',
+			description: 'Test your knowledge with quizzes and assessments.',
+			actionText: 'Start Quiz',
+			icon: QuizIcon,
+			onActionClick: () => {
+				// Handle click event for Quizzes and Assessments
+			},
+		},
+		{
+			title: 'Market Sentiment Analysis',
+			description: 'Stay updated with real-time market sentiment analysis.',
+			icon: ShowChartIcon,
+			actionText: 'View Analysis',
+			onActionClick: () => {
+				// Handle click event for Market Sentiment Analysis
+			},
+		},
+	];
 
-  const stories = [
-    {
-      title: 'Market Hits New Highs',
-      description: 'The stock market reached new highs today, with tech stocks leading the way.',
-    },
-    {
-      title: 'Economic Growth Forecast',
-      description: 'Experts predict steady economic growth over the next quarter.',
-    },
-    // Add more stories as needed
-  ];
+	const stories = [
+		{
+			title: 'Market Hits New Highs',
+			description:
+				'The stock market reached new highs today, with tech stocks leading the way.',
+		},
+		{
+			title: 'Economic Growth Forecast',
+			description:
+				'Experts predict steady economic growth over the next quarter.',
+		},
+		// Add more stories as needed
+	];
 
-  return (
-    <>
-      <AppBar />
-      <TradingViewTicker />
-      <Box
-        sx={{
-          display: 'flex',
-          marginTop: '20px',
-          minHeight: '100vh',
-          alignItems: 'flex-start',
-        }}
-      >
-        {/* <Sidebar stories={stories} /> */}
+	return (
+		<>
+			<AppBar />
+			<TradingViewTicker />
+			<Box
+				sx={{
+					display: 'flex',
+					marginTop: '20px',
+					minHeight: '100vh',
+					alignItems: 'flex-start',
+				}}
+			>
+				{/* <Sidebar stories={stories} /> */}
 
-        <TradingViewTimeline />
-        <Container
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flexGrow: 1,
-            marginLeft: '450px',
-            marginTop: '-720px', 
-          }}
-        >
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={2}
-          >
-            {tileData.map((tile, index) => (
-              <Grid item xs={12} sm={6} key={index}>
-                <Tile {...tile} />
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </Box>
-    </>
-  );
+				<TradingViewTimeline />
+				<Container
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						flexGrow: 1,
+						marginLeft: '450px',
+						marginTop: '-720px',
+					}}
+				>
+					<Grid
+						container
+						justifyContent="center"
+						alignItems="flex-start"
+						spacing={2}
+					>
+						{tileData.map((tile, index) => (
+							<Grid item xs={12} sm={6} key={index}>
+								<Tile {...tile} />
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</Box>
+		</>
+	);
 };
 
 export default Home;
